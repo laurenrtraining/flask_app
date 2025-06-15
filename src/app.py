b0871b713e35fb81d155e7957852578fa1008aab
lauren r <laurenr.training@gmail.com>
2025-06-15 17:47:18 +0100

    Add comments to app.py for easier understanding

--- src/app.py
+++ src/app.py
@@ -6,12 +6,9 @@ from datetime import date, timedelta
 from collections import defaultdict
 
 instance_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'instance')) 
-# needed so the instance isn't made within src
+# I needed to specify the route so the database isn't made in the wrong file
 
 app = Flask(__name__, instance_path=instance_path, instance_relative_config=True, static_folder='static', template_folder='templates')
-
-# In memory calendar store for demo
-user_availability = {} 
 
 # Ensures any uploaded images can be accessed when creating new groups
 UPLOAD_FOLDER = 'static/group_images'
@@ -26,17 +23,20 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 app.secret_key = os.urandom(24)
-# Creating a secret key to run sessions for log ins and sign essential cookies on the website
+# Creating a secret key to run login sessions
 # Random creation for added security
 
-#Creating homepage
-# Add the HTML webpage designs
+
+### App.routes for rendering all pages ###
+### REGISTRSTION AND SIGN IN ###
+
+# Render homepage
 @app.route('/')
 def index():
     groups = Societies.query.all()
     return render_template('index.html', groups=groups)
 
-
+# Render sign in page
 @app.route('/sign_in.html', methods=['GET','POST'])
 def sign_in():
     if request.method == 'POST':
@@ -53,7 +53,7 @@ def sign_in():
         
     return render_template('sign_in.html')
 
-
+# Render regsistration page
 @app.route('/register.html', methods=['GET', 'POST'])
 def registration():
     if request.method == 'POST':
@@ -85,12 +85,15 @@ def registration():
     
     return render_template('register.html')
 
-# testing html page renders
-
 @app.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('index'))
+
+
+
+
+### RENDERING NAVBAR LINKS ###
 
 @app.route('/about')
 def about():
@@ -103,21 +106,25 @@ def my_groups():
         return render_template('my_groups.html', user_id=None, groups=[])
 
     memberships = Staff_Societies.query.filter_by(staff_id=user_id).all()
-    # checks the Staff_Societies db for where the current logged in staff matched
+    # checks Staff_Societies db for where the current logged in staff matcheS
     society_ids = [x.society_id for x in memberships]
-    # Loops through Staff Societies and finds the Society_id for each of them
+    # Loops through Staff_Societies and finds the Society_id for each of them
 
     user_societies = Societies.query.filter(Societies.society_id.in_(society_ids)).all()
-    # groups all the ids from the previous check together to be called
+    # groups all the society ids together to be called
 
     return render_template('my_groups.html', user_id=user_id, groups=user_societies)
 
+
+
+### CREATING NEW GROUPS/SOCIETIES ###
 
 # Creation of new groups
 @app.route('/create-group')
 def create_group():
     return render_template('create_group.html')
 
+# Saves new group to database and renders it on the homepage
 @app.route('/submit-group', methods=['POST'])
 def submit_group():
     name = request.form.get('name')
@@ -141,9 +148,10 @@ def submit_group():
 
     db.session.add(new_group)
     db.session.commit()
-
-    
     return redirect(url_for('index'))
+
+
+### CODE BASE FOR ALL NEW SOCIETIES THAT ARE MADE ###
 
 @app.route('/group/<int:group_id>', methods=['GET', 'POST'])
 def group_detail(group_id):
@@ -160,14 +168,12 @@ def group_detail(group_id):
 
     error = None  # default no error
 
-    # Is user already a member of this group
-    is_member = False
+    # Is user already a member of this group?
+    is_member = False # Pre-set
 
     if user_id:
         is_member = Staff_Societies.query.filter_by(staff_id=user_id, society_id=group_id).first()
     
- 
-
     if request.method == 'POST':
         if 'user_id' not in session:
             # If the user isn't logged in this error will be thrown
@@ -211,6 +217,9 @@ def group_detail(group_id):
         error=error
     )
 
+
+### DELETING GROUPS FROM THE DATABASE ###
+
 @app.route('/group/<int:group_id>/delete', methods=['POST'])
 def delete_group(group_id):
     group = Societies.query.get_or_404(group_id)
@@ -227,6 +236,9 @@ def delete_group(group_id):
     db.session.delete(group)
     db.session.commit()
     return redirect(url_for('index'))  # Return to homepage
+
+
+### JOIN AND LEAVE GROUPS ###
 
 # Join Group
 @app.route('/group/<int:group_id>/join', methods=['POST'])
@@ -247,6 +259,7 @@ def join_group(group_id):
     db.session.commit()
     return redirect(url_for('group_detail', group_id=group_id))
 
+# Leave group
 @app.route('/group/<int:group_id>/leave', methods=['POST'])
 def leave_group(group_id):
     user_id = session.get('user_id')
@@ -262,8 +275,8 @@ def leave_group(group_id):
     return redirect(url_for('group_detail', group_id=group_id))
 
 
-# Checking if the database already exists
 
+# RUNS THE APPLICATION AND CHECKS DATABASE EXISTENCE #
 if __name__ == '__main__':
     os.makedirs(app.instance_path, exist_ok=True)
     with app.app_context():
